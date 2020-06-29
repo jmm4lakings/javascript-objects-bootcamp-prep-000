bbd856b0712ebdc4ce86f09179132f333394229e
Justin Mayer <justin_mayer2003@yahoo.com>
2020-06-29 23:56:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,12 +1,6 @@
-var playlist = { 'song': 'title'}
 
-var updatePlaylist = function(playlist, artistName, songTitle){
-   return Object.assign({}, "MyBloodyValentine", { ["PhilOchs"]: "Slowdive" })
-}
+var playlist = new Object ({});
 
-var removeFromPlaylist = function(playlist, artistName){
-  var playlist = playlist;
-  delete playlist.artistName
-  return playlist
-}
-
+var updatedPlaylist = function(playlist, artistName, songTitle){
+  return Object.assign({}, playlist, {[artistName]: songTitle})
+};
