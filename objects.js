56cc30a6ac3da207fcde705fd0b3e205f1322aba
Justin Mayer <justin_mayer2003@yahoo.com>
2020-06-29 23:01:59 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,4 @@ var meals = ( {} );
 var meals = new Object({});
 var meals = { breakfast: "oatmeal" };
 var meals = new Object( {breakfast: 'oatmeal'})
-var meals = {
-  breakfast: 'eggs',
-  breakfast: 'bacon'
-}
+
