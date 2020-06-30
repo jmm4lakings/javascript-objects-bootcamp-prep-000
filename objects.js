76099bd99bedfc4c226c464ca7dbdb3bffbb7107
Justin Mayer <justin_mayer2003@yahoo.com>
2020-06-30 00:47:18 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = ( {'song': 'title'});
 
-  function updatePlaylist(playlist, artistName, songTitle) {
+  function updatePlaylist({ playlist, artistName, songTitle }) {
   playlist[artistName] = songTitle
   return playlist
